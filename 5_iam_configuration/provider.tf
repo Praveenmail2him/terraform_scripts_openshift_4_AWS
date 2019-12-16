1dda33f8009c7caca3e4bcf62c12bb7d507161d9
PraveenKumar KrishnaiyerSrinivasan <praveenkumar.k11@ad.infosys.com>
2019-12-16 09:02:30 +0530

    Terraform Customized Templates for AWS UPI Installation

--- 5_iam_configuration/provider.tf
+++ 5_iam_configuration/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "${var.aws_region}"
-}
+ }
 
 data "aws_caller_identity" "current" {
 }
--- 7_control_plane_RHCS_configuration/provider.tf
+++ 7_control_plane_RHCS_configuration/provider.tf
@@ -1,12 +1,19 @@
 provider "aws" {
   region = "${var.aws_region}"
-}
+  }
 
 data "aws_caller_identity" "current" {
 }
 
 resource "random_id" "clusterid" {
   byte_length = "2"
+}
+
+data "aws_region" "current" {}
+
+data "aws_availability_zone" "aws_azs" {
+  count = "${length(var.aws_azs)}"
+  name = "${format("%s%s", data.aws_region.current.name, element(var.aws_azs, count.index))}"
 }
 
 locals {
