d8c067bc0dcd0b6de8a9de63dc7d810147762454
Praveen Kumar KS <19602362+Praveenmail2him@users.noreply.github.com>
2020-01-20 21:56:44 +0530

    Update Variables.tf for RHCOS

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "private_vpc_cidr" { default = "10.10.0.0/16" }
 variable "domain" { }
 variable "vpc_private_subnet_cidrs" { default = ["10.10.10.0/24","10.10.11.0/24","10.10.12.0/24"] }
 variable "vpc_public_subnet_cidrs" { default = ["10.10.20.0/24","10.10.21.0/24","10.10.22.0/24"] }
-variable "ami" { default = "ami-0bc59aaa7363b805d" }
+variable "ami" { default = "ami-005141ec3f197b7be" }
 variable "aws_access_key_id" {  }
 variable "aws_secret_access_key" {  }
 variable "cluster_network_cidr" { default = "192.168.0.0/17" }
